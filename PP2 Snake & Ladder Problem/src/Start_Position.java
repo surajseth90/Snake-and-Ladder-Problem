6adacf40e4daac6f77977a69d498edfef4f0883a
surajseth90 <surajseth90@gmail.com>
2021-04-17 22:05:51 +0530

    Code to reach at the winning position is added successfully

--- PP2 Snake & Ladder Problem/src/Start_Position.java
+++ PP2 Snake & Ladder Problem/src/Start_Position.java
@@ -3,23 +3,33 @@ public class Start_Position {
 
 	public static void main(String[] args) {
 		int player = 0;
+		
+		while(player!=100)
+		{
 		int die = ThreadLocalRandom.current() .nextInt(1,7);
 		int check_option = ThreadLocalRandom.current() .nextInt(0,3);
 		if(check_option==0)
 		{
-			player = player+0;
+			player = player ;
 		}
 		else if(check_option==1)
 		{
-			player = player +die;
+			player = player + die;
 		}
 		else
 		{
-			 player = player -die;
+			 player = player - die;
 			if(player>0)
 				player = player;
 			else
 				player =0;
+		}
+		if(player==100)
+		{
+			break;
+		}
+			
+		
 		}
 	}
 }
