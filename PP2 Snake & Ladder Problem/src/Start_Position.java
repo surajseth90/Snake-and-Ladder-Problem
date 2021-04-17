0ad51a4499855d4c7ffc094407103a4ed621bfa0
surajseth90 <surajseth90@gmail.com>
2021-04-17 18:57:46 +0530

    Generate a number between 0 to 6 successfully

--- PP2 Snake & Ladder Problem/src/Start_Position.java
+++ PP2 Snake & Ladder Problem/src/Start_Position.java
@@ -3,14 +3,9 @@ public class Start_Position {
 
 	public static void main(String[] args) {
 		int player = 0;
-		int Actual_die ;
-		//int die =(int) (Math.random()*10);
-		
-			for(int i=0; i<100;i++)
-			{
-				int die = ThreadLocalRandom.current() .nextInt(1,7);
-				System.out.println(die);
-			}
+		int die = ThreadLocalRandom.current() .nextInt(1,7);
+				
+			
 		
 		     
 		      
