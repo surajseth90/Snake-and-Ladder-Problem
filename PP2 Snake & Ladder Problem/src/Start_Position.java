95ead1765079b362459cd43e95c0816a70ee5e62
surajseth90 <surajseth90@gmail.com>
2021-04-17 19:49:46 +0530

    Code to check the next move is added successfully

--- PP2 Snake & Ladder Problem/src/Start_Position.java
+++ PP2 Snake & Ladder Problem/src/Start_Position.java
@@ -4,18 +4,14 @@ public class Start_Position {
 	public static void main(String[] args) {
 		int player = 0;
 		int die = ThreadLocalRandom.current() .nextInt(1,7);
-		System.out.println("die: "+die);
 		int check_option = ThreadLocalRandom.current() .nextInt(0,3);
-		System.out.println("option: "+check_option);
 		if(check_option==0)
 		{
 			player = player+0;
-			System.out.println(player);
 		}
 		else if(check_option==1)
 		{
 			player = player +die;
-			System.out.println(player);
 		}
 		else
 		{
