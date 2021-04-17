33046e6ccb12e9d7735cedc9669e0c19640286d2
surajseth90 <surajseth90@gmail.com>
2021-04-17 23:07:39 +0530

    Code to report number of times dice played, is added

--- PP2 Snake & Ladder Problem/src/Start_Position.java
+++ PP2 Snake & Ladder Problem/src/Start_Position.java
@@ -1,39 +1,51 @@
+
+import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 public class Start_Position {
 
 	public static void main(String[] args) {
-		int player = 0;
+		int Position_of_Player = 0;
+		int n=0;
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+
 		
-		while(player!=100)
+		while(Position_of_Player!=100)
 		{
 		int die = ThreadLocalRandom.current() .nextInt(1,7);
 		int check_option = ThreadLocalRandom.current() .nextInt(0,3);
 		if(check_option==0)
 		{
-			player = player ;
+			Position_of_Player = Position_of_Player ;
+			arr.add(n,Position_of_Player);
 		}
 		else if(check_option==1)
 		{
-			player = player + die;
+			Position_of_Player = Position_of_Player + die;
+			arr.add(n,Position_of_Player);
 		}
 		else
 		{
-			 player = player - die;
-			if(player>0)
-				player = player;
+			 Position_of_Player = Position_of_Player - die;
+			if(Position_of_Player>0)
+				Position_of_Player = Position_of_Player;
 			else
-				player =0;
+				Position_of_Player =0;
+			arr.add(n,Position_of_Player);
 		}
-		if(player >100)
+		if(Position_of_Player >100)
 		{
-			player = player - die;
+			Position_of_Player = Position_of_Player - die;
 		}
-		if(player==100)
+		if(Position_of_Player==100)
 		{
+			
+			System.out.println("Dice was played "+(n+1) +
+					" times and player reached at the winning position "+Position_of_Player);
 			break;
 		}
-			
-		
+		System.out.println("Dice was played "+(n+1) +
+				" times and player reached at the position "+Position_of_Player);
+		n++;
 		}
 	}
 }
